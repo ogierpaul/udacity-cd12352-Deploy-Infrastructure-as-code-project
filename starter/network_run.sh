5a5e6cc8a11a2d2322ed919604a5d67d027b4dff
ogierpaul <ogier.paul@gmail.com>
2024-02-16 21:11:33 +0100

    Substituting environment variables in the parameters file

--- starter/network_run.sh
+++ starter/network_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ## Check pre-requisites
-source ./test_pre_requisites.sh
+# source ./test_pre_requisites.sh
 
-## Calculate stackname
+## Calculate stackname and project name and add them as environment variables
 NETWORKSTACKNAME=$(echo "$STACKNAMESPACE-$STACKENV-NETWORK" | tr '[:upper:]' '[:lower:]')
+PROJECTNAME=$(echo "$STACKNAMESPACE-$STACKENV" | tr '[:upper:]' '[:lower:]')
 
 ## Create or update network
-source ./run.sh --stackname $NETWORKSTACKNAME --template network/network.yml --parameters network/network-parameters.json
+source ./run.sh --stackname $NETWORKSTACKNAME --template network/network.yml --parameters network/network-parameters.json --projectname $PROJECTNAME
--- starter/run.sh
+++ starter/run.sh
@@ -9,16 +9,17 @@
 STACKNAME="$STACKNAME"
 TEMPLATE="$TEMPLATE"
 PARAMETERS="$PARAMETERS"
+PROJECTNAME="$PROJECTNAME"
 
 # Function to display usage instructions
 usage() {
-    echo "Usage: $0 [--stackname <stackname>] [--template <template>] [--parameters <parameters>]"
-    echo "       $0 <stackname> <template> <parameters>"
+    echo "Usage: $0 [--stackname <stackname>] [--template <template>] [--parameters <parameters>] [--projectname <projectname>]"
+    echo "       $0 <stackname> <template> <parameters> <projectname>"
     exit 1
 }
 
 # Check if the number of arguments is correct
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
     # Parse command-line arguments
   while [[ $# -gt 0 ]]; do
       key="$1"
@@ -35,6 +36,10 @@ if [ $# -ne 3 ]; then
               PARAMETERS="$2"
               shift 2
               ;;
+          --projectname)
+              PROJECTNAME="$2"
+              shift 2
+              ;;
           *)
               # If an invalid flag is provided or positional parameters are used, display usage instructions
               usage
@@ -45,14 +50,19 @@ fi
 
 
 # If any required parameter is missing, display usage instructions
-if [[ -z $STACKNAME || -z $TEMPLATE || -z $PARAMETERS ]]; then
+if [[ -z $STACKNAME || -z $TEMPLATE || -z $PARAMETERS || -z $PROJECTNAME ]]; then
     usage
 fi
 
 echo "Stackname: $STACKNAME"
 echo "Template: $TEMPLATE"
 echo "Parameters: $PARAMETERS"
+echo "Project name: $PROJECTNAME"
 
+
+# Substituting environment variables in the JSON file
+echo "Creating parameter file with environment variables in temp/ folder"
+envsubst < $PARAMETERS > temp/$STACKNAME-parameters.json
 
 echo "Start check if stackname $STACKNAME  exists in region $REGION"
 
@@ -63,12 +73,11 @@ if ! aws cloudformation describe-stacks --region "$REGION" --stack-name "$STACKN
   aws cloudformation create-stack \
   --stack-name "$STACKNAME" \
   --template-body "file://$TEMPLATE" \
-  --parameters="file://network/network-parameters.json" \
-  --region=$REGION   
+  --parameters="file://temp/$STACKNAME-parameters.json" \
+  --region=$REGION 
   
   echo "Waiting for stack to be created ..."
   aws cloudformation wait stack-create-complete --stack-name $STACKNAME --region=$REGION
-
 else
 
   echo -e "\nStack exists, attempting update ..."
@@ -83,7 +92,7 @@ else
   update_output=$( aws cloudformation update-stack \
     --stack-name $STACKNAME \
     --template-body file://$TEMPLATE \
-    --parameters file://$PARAMETERS \
+    --parameters file://temp/$STACKNAME-parameters.json \
     --region=$REGION 2>&1)
   
   status=$?
@@ -109,5 +118,6 @@ else
   aws cloudformation wait stack-update-complete --stack-name $STACKNAME --region=$REGION
 
 fi
-
+echo "Removing parameter file in temp/ folder"
+rm temp/$STACKNAME-parameters.json
 echo "Finished creating or updating stack successfully!"
